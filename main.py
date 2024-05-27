62c6d44036698bdab187f6a569dd91d0c2f28814
alex-v-10 <alex.ru9311@gmail.com>
2024-05-27 12:17:29 +0300

    refactor fucntions

--- main.py
+++ main.py
@@ -15,40 +15,46 @@ loop_count = 0
 
 client = TelegramClient('session_name', api_id, api_hash)
 
-async def fetch_messages():
-    await client.start(phone_number)
-    
-    all_messages = []
-    last_message_ids = {}
-    differences = {}
-
-    if os.path.exists('data/last_message_ids.json'):
-        with open('data/last_message_ids.json', 'r', encoding='utf-8') as f:
-            last_message_ids = json.load(f)
-            
+async def get_unread_counts(last_message_ids):
+    unread_counts = {}
     for channel_username in channel_usernames:
         channel = await client.get_entity(channel_username)
         async for message in client.iter_messages(channel, limit=1):
             message_id = message.id
             last_message_id = last_message_ids.get(channel_username, message_id)
             difference = message_id - last_message_id
-            differences[channel_username] = difference
+            unread_counts[channel_username] = difference
             last_message_ids[channel_username] = message_id
-    
+    return unread_counts
+  
+async def get_new_messages(unread_counts):
+    new_messages = []
     for channel_username in channel_usernames:
-        if differences[channel_username] > 0:
-            channel = await client.get_entity(channel_username)
-            async for message in client.iter_messages(channel, limit=differences[channel_username]):
-                message_data = {
-                    'channel': channel_username,
-                    'date': message.date.isoformat(),
-                    'message_id': message.id,
-                    'text': message.text
-                }
-                all_messages.append(message_data)
+          if unread_counts[channel_username] > 0:
+              channel = await client.get_entity(channel_username)
+              async for message in client.iter_messages(channel, limit=unread_counts[channel_username]):
+                  message_data = {
+                      'channel': channel_username,
+                      'date': message.date.isoformat(),
+                      'message_id': message.id,
+                      'text': message.text
+                  }
+                  new_messages.append(message_data)
+    return new_messages
+
+async def fetch_messages():
+    await client.start(phone_number)
+    
+    last_message_ids = {}
+    if os.path.exists('data/last_message_ids.json'):
+        with open('data/last_message_ids.json', 'r', encoding='utf-8') as f:
+            last_message_ids = json.load(f)
+    
+    unread_counts = await get_unread_counts(last_message_ids)
+    new_messages = await get_new_messages(unread_counts)
             
-    with open('data/messages.json', 'w', encoding='utf-8') as f:
-        json.dump(all_messages, f, ensure_ascii=False, indent=4)
+    with open('data/new_messages.json', 'w', encoding='utf-8') as f:
+        json.dump(new_messages, f, ensure_ascii=False, indent=4)
         
     with open('data/last_message_ids.json', 'w', encoding='utf-8') as f:
         json.dump(last_message_ids, f, ensure_ascii=False, indent=4)
